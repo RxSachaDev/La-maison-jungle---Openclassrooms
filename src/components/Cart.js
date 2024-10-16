c18a01bfd4e69e0dd9f893c13ea9ac7bdd3c908f
ROUX SACHA <sacha.work38@gmail.com>
2024-10-16 21:49:33 +0200

    Manipulation de useeffect

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
@@ -7,6 +7,12 @@ function Cart({ cart, updateCart }) {
 		(acc, plantType) => acc + plantType.amount * plantType.price,
 		0
 	)
+	localStorage.getItem(cart)
+
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
+
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
